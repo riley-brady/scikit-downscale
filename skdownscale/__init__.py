e9c8144ebda18df35c4eb102e81868c23bfe5834
Riley Brady <riley_brady@mckinsey.com>
2023-06-12 16:37:25 -0600

    release as version 1.0.0 with numpy.object fix

--- skdownscale/__init__.py
+++ skdownscale/__init__.py
@@ -1,7 +1,1 @@
-from pkg_resources import DistributionNotFound, get_distribution
-
-try:
-    __version__ = get_distribution(__name__).version
-except DistributionNotFound:  # noqa: F401
-    # package is not installed
-    __version__ = '0.0.0'  # Fixme
+__version__ = "1.0.0"
